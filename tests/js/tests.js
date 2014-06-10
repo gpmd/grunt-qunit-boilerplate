82747369a45b6f998817ce2f41351b9db411fa92
Matt Bailey <matt@gpmd.co.uk>
2014-06-10 15:02:19 +0100

    Bugfix

--- tests/js/tests.js
+++ tests/js/tests.js
@@ -4,7 +4,7 @@
     module("Basic Tests");
 
     test("truthy", function() {
-        ok(false, "true is truthy");
+        ok(true, "true is truthy");
         equal(1, true, "1 is truthy");
         notEqual(0, true, "0 is NOT truthy");
     });
